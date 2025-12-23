ab81e07e3e94dfae60b422d5408dccdb91a80966
pietromontanticomposer <pietromontanticomposer@gmail.com>
2025-12-23 17:55:12 +0100

    Remove large assets and local artifacts from repo

--- check-token.cjs
+++
@@ -1,3 +0,0 @@
-require('dotenv').config({ path: '.env.local' });
-console.log('hasToken', Boolean(process.env.BLOB_READ_WRITE_TOKEN));
-console.log((process.env.BLOB_READ_WRITE_TOKEN || '').slice(0, 25));
